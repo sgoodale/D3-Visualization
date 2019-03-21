5cd33434013732a0b44b33380675072b36ab0804
chelseardinh <chelseadinh94@gmail.com>
2019-03-20 21:02:03 -0700

    removed json file and refactored data.

--- parse_data.py
+++ parse_data.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 #Read in original data file.
 raw_data = pd.read_csv("Patient_Characteristics_Survey_PCS_2015.csv")
 
-print("\nNumber of instances and columns before preprocessing data:\n")
-print(raw_data.shape)
+print("\nNumber of instances and columns before preprocessing data: "+str(raw_data.shape)+"\n")
 
 #Replace "UNKNOWN" values with NaN.
 raw_data = raw_data.replace("UNKNOWN", np.nan)
@@ -16,16 +15,11 @@ raw_data = raw_data.replace("UNKNOWN", np.nan)
 #Randomize data
 raw_data.reindex(np.random.permutation(raw_data.index))
 
-#Replace val OTHER in column 'Race' with Hispanic if applicable
-for index, row in raw_data.iterrows():
-	if raw_data.at[index, 'Hispanic Ethnicity'] == 'YES':
-		raw_data.at[index, 'Race'] = 'HISPANIC'
-
 #Drop all rows that have NaN values in at least one cell.
 raw_data = raw_data.dropna()
 
-#Transfer data to new CSV
-raw_data.to_csv('refactored_data.csv', index=False)
+#Group values by attributes
+racial_groups = raw_data.groupby('Race')
 
 #Condense data to only include the following attributes:
 raw_data = raw_data[['Mental Illness','Alcohol Related Disorder','Drug Substance Disorder',
@@ -36,11 +30,7 @@ raw_data = raw_data[['Mental Illness','Alcohol Related Disorder','Drug Substance
 raw_data = raw_data.replace("NO", 0.0)
 raw_data = raw_data.replace("YES", 1.0)
 
-print("\nNumber of instances and columns after processing:\n")
-print(raw_data.shape)
-
-#Transfer data to new CSV
-raw_data.to_csv('parsed_data.csv', index=False)
+print("\nNumber of instances and columns after processing: "+str(raw_data.shape)+"\n")
 
 #Get resulting correlation matrix:
 corr_matrix = raw_data.corr()
@@ -62,6 +52,9 @@ plt.yticks(label_position, labels)
 plt.xticks(label_position, labels)
 plt.xticks(rotation=90)
 
-#plot correlation matrix as heat map
+#Plot correlation matrix as heat map
 plt.imshow(corr_matrix, cmap='hot', interpolation='nearest')
 plt.show()
+
+#Transfer data to new CSV
+raw_data.to_csv('parsed_data.csv', index=False)
